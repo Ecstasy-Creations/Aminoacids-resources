2f5f4471ba47259961b72cdd22c6b9ffed17b290
Ecstasy-Creations <aminalhafi2006@gmail.com>
2026-06-28 14:20:33 -0400

    Fix library resource links

--- resources.js
+++ resources.js
@@ -23,9 +23,7 @@ const resources = [
   {
     title: "Library Research Databases",
     category: "Study Tools",
-    description: " 1) <a href=https://annas-archive.gl/> Anna's archive, the largest open Library online</a>. <a href=https://scholar.google.com/> Google scholar</a> <a href=https://openstax.org/subjects> openstacks for free textbooks</a> 
-    
-    .",
+    description: "1) <a href=\"https://annas-archive.gl/\">Anna's Archive, the largest open library online</a>. 2) <a href=\"https://scholar.google.com/\">Google Scholar</a>. 3) <a href=\"https://openstax.org/subjects\">OpenStax for free textbooks</a>.",
     url: "#",
     tag: "Campus"
   },
